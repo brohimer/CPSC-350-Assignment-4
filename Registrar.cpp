b8f53bbce41a8df5f8d84e8ec6b24abea1ed920c
oliver <oliver@pop-os.localdomain>
2020-04-14 13:46:36 -0700

    registrar class created and filled up to what we need atm

---
+++ Registrar.cpp
@@ -0,0 +1,24 @@
+#include "Registrar.h"
+
+Registrar::Registrar()
+{
+  current_tick = 0;
+  windows_open = 0;
+  students_waiting = 0;
+}
+
+Registrar::~Registrar()
+{
+
+}
+
+void Registrar::run_simulation()
+{
+  //do a while loop through the file
+  //and skip every 3 i Guess
+}
+
+void Registrar::update_students_waiting()
+{
+  //students_waiting = student_queue->size();
+}
---
+++ Registrar.h
@@ -0,0 +1,19 @@
+class Registrar
+{
+
+//Registrar Class:
+//.Current input file line, next group(), .current_time
+
+  public:
+    Registrar(); //Default constructor
+    ~Registrar(); //Destructor
+
+    void run_simulation();
+    void update_students_waiting();
+
+  private:
+    int current_tick;
+    int windows_open;
+    int students_waiting;
+
+};
