61c5d465b4495770684fce0b2914577e211f9082
oliver <oliver@pop-os.localdomain>
2020-04-15 13:51:56 -0700

    fixed window and registar bases

--- Window.cpp
+++ Window.cpp
@@ -3,6 +3,7 @@
 Window::Window()
 {
   Occupied = false;
+  total_idle_time = 0;
 }
 
 Window::~Window()
@@ -22,4 +23,9 @@ void Window::setOccupied()
 void Window::setUnoccupied()
 {
   Occupied = false;
+}
+
+void Window::increment_idle_time()
+{
+  total_idle_time += 1;
 }
